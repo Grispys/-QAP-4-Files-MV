97c25c7e0d150a217a537afa932c59c207823f69
Grispys <162459734+Grispys@users.noreply.github.com>
2024-03-21 12:28:57 -0230

    Delete intputFunc.py

--- intputFunc.py
+++
@@ -1,6 +0,0 @@
-def intput(prompt="Enter an integer: "):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("error - must be integer")
